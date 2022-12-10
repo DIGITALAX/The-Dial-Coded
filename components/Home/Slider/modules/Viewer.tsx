b7c5a981fdec3059a8a212ecafad750f4962804e
emmajane1313 <admin@digitalax.xyz>
2022-12-10 17:19:06 -0500

    slider refactor

--- components/Home/Slider/modules/Viewer.tsx
+++ components/Home/Slider/modules/Viewer.tsx
@@ -1,19 +1,20 @@
 import Image from "next/legacy/image";
 import { FunctionComponent } from "react";
-import { DropsViewerProps } from "./../types/slider.types";
+import { ViewerProps } from "./../types/slider.types";
 
-const DropsViewer: FunctionComponent<DropsViewerProps> = ({
-  dropsSlider,
+const Viewer: FunctionComponent<ViewerProps> = ({
+  slider,
+  width,
 }): JSX.Element => {
   return (
     <div className="relative w-full h-full col-start-2 col-span-10 gap-3 overflow-x-scroll flex">
-      {dropsSlider?.map((uri: string, index: number) => {
+      {slider?.map((uri: string, index: number) => {
         return (
           <div
             key={index}
             className={`relative w-full object-cover rounded-2xl h-96`}
           >
-            <div className="relative w-60 h-full rounded-2xl">
+            <div className={`relative w-${width} h-full rounded-2xl`}>
               <Image
                 src={`https://thedial.infura-ipfs.io/ipfs/${uri}`}
                 layout="fill"
@@ -29,4 +30,4 @@ const DropsViewer: FunctionComponent<DropsViewerProps> = ({
   );
 };
 
-export default DropsViewer;
+export default Viewer;
