32a2a584c91b57efe3c589e982865a465d6ec9e8
emmajane1313 <admin@digitalax.xyz>
2023-01-01 09:58:37 -0500

    fix props

--- components/Home/Post/types/post.types.ts
+++ components/Home/Post/types/post.types.ts
@@ -34,5 +34,5 @@ export type CommentsProps = {
   handleRemoveGif: (result: any) => void;
   gifs: string[];
   reactionsFeed: any[];
-  handleHidepost: (id: string) => Promise<void>;
+  handleHidePost: (id: string) => Promise<void>;
 };
