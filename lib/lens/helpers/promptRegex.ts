62c0afe04e2db8428f4822b9414a726ca2a6a812
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-01-02 22:43:56 -0500

    index

--- lib/lens/helpers/promptRegex.ts
+++ lib/lens/helpers/promptRegex.ts
@@ -1,7 +1,7 @@
 const promptRegex = (prompt: string): string | undefined => {
   const splitPrompt: string[] = prompt.split(" ");
   const pattern =
-    /\b(and|the|a|of|,|at|but|then|because|as|so|if|or|yet|whereas|unless|since|either)\b/i;
+    /\b(and|the|a|of|an|,|at|but|then|because|as|so|if|or|yet|whereas|unless|since|either|other|however|in|i|for|found|on|while|found)\b/i;
   for (let i = 0; i < splitPrompt?.length; i++) {
     if (!splitPrompt[i]?.match(pattern)) {
       const returnedResult = splitPrompt[i].replace(/,/g, "");
