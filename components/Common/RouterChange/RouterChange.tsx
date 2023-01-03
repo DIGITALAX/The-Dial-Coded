8142e66b607eb044176b7ace3a7daeb461aeea0b
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-01-02 21:35:05 -0500

    router change load

---
+++ components/Common/RouterChange/RouterChange.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { FunctionComponent } from "react";
+import { INFURA_GATEWAY } from "../../../lib/lens/constants";
+
+const RouterChange: FunctionComponent = (): JSX.Element => {
+  return (
+    <div className="relative w-screen h-screen grid grid-flow-col auto-cols-auto bg-white/80">
+      <div className="relative animate-spin w-20 h-20 place-self-center col-start-1">
+        <Image
+          src={`${INFURA_GATEWAY}/ipfs/QmQZ8UwjeizDQkbCiZED8Ya4LxpFD5JbVbNeAdowurHkiY`}
+          className="relative w-fit h-fit relative cursor-pointer"
+          width={100}
+          height={100}
+          alt="dial"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RouterChange;
