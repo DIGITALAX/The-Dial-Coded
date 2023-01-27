959c59ce5b2c23840e6e5ebbf63c17d20581562c
emmajane1313 <admin@digitalax.xyz>
2023-01-27 06:12:02 -0500

    preload fonts + head

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,49 @@ const Home: NextPage<HomeProps> = ({ newLink }): JSX.Element => {
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:site_name" content="The Dial" />
         <meta property="og:image" content="https://thedial.xyz/card.png/" />
+        <meta name="og:title" content="The Dial" />
         <meta property="og:type" content="website" />
+        <meta name="twitter:image" content="https://thedial.xyz/card.png/" />
+        <meta name="twitter:url" content="https://thedial.xyz/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:site" content="@digitalax" />
+        <meta name="twitter:creator" content="@digitalax" />
+        <link
+          rel="preload"
+          href="/fonts/DosisRegular.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/DS-DIGI.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/DS-DIGIT.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/DS-DIGII.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/DS-DIGIB.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
       </Head>
       <Scan newLink={newLink} />
       <LayoutSwitch />
