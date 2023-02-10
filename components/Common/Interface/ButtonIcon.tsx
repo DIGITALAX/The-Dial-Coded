9d7c34640131dd455d4e7cc95d04ba4b653c3218
emmajane1313 <admin@digitalax.xyz>
2023-02-10 02:25:23 -0500

    fix button size

--- components/Common/Interface/ButtonIcon.tsx
+++ components/Common/Interface/ButtonIcon.tsx
@@ -13,7 +13,7 @@ const ButtonIcon: FunctionComponent<ButtonIconProps> = ({
 }): JSX.Element => {
   return (
     <div
-      className={`relative w-fit col-start-${col} border-4 w-${width} h-${height} border-black bg-yell grid grid-flow-row auto-rows-auto p-px self-${self} justify-self-${justify}`}
+      className={`relative col-start-${col} border-4 w-${width} h-${height} border-black bg-yell grid grid-flow-row auto-rows-auto p-px self-${self} justify-self-${justify}`}
     >
       {image && (
         <Image
